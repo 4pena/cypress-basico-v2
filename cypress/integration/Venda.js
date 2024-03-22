a55a290ac437a23e6bb2bfd68d130ab777d7ca15
4pena <gabrielpenabr2@gmail.com>
2024-03-22 12:49:34 -0300

    Commit 22/03/24

--- cypress/integration/Venda.js
+++ cypress/integration/Venda.js
@@ -15,3 +15,15 @@ it('Venda PDV 5', function() {
     cy.wait(7000)
     cy.get('.txt-estado').should('have.text', 'Caixa livre');
 })
+
+it.only('Teste Flag', function(){
+
+    cy.MAN_Acessar_Manager()
+    cy.visit('http://10.245.0.12/manager/cad_devolucao.php5')
+      cy.get('#FLT_COD_LOJA').select('1')
+      cy.get('#FLT_DATA_CADASTRO').clear().type('{enter}')
+      //cy.contains('label', 'Devolução Emitida para Loja Origem').click();
+      cy.get('#FLT_FLG_LOJA_ORIGEM').check({ force: true }).should('be.checked')
+      //cy.get('#EXEC_FILTRO_CUSTOM').click()
+      //cy.wait(2000)
+})
